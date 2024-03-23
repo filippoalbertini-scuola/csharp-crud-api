8b4a144e404bcac853b0694358dd3d108b23b42a
Filippo Albertini <filippo@filippoalbertini.it>
2024-03-23 07:53:09 +0100

    docs: add comment to use HttpClient provider

--- app-weather/src/main.ts
+++ app-weather/src/main.ts
@@ -2,9 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from "@angular/common/http";
-// bootstrapApplication(AppComponent, appConfig)
-//   .catch((err) => console.error(err));
 
+// https://this-is-angular.github.io/angular-guides/docs/standalone-apis/using-the-httpclient-in-a-standalone-angular-application
 bootstrapApplication(AppComponent, {
   providers: [provideHttpClient()],
 }).catch((err) => console.error(err));
